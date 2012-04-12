afb4fb5fa07a6109304689ccabc061ddb6178f66
Stefan Dirnstorfer <stefandirnstorfer@gmail.com>
2012-04-12 09:38:24 +0000

    drag and drop fixed

--- touched.js
+++ touched.js
@@ -1,8 +1,24 @@
+// DnD frame work
+// hand is a reference to the object currently beeing dragged.
+var hand= null;
+
+// The screen coordinates of the last drag update.
+var startPos;
+var startOffset;
+var hasMoved= false;
+var unselect= false;
+
+console.log('START');
+
 $(init);
 function init() {
+    var canvas= $('#canvas');
+    $('body').mousemove(msMove);
+    $('body').mouseup(msUp);
+    $('body').mousedown(function(evt) { evt.preventDefault(); });
     var start= bodyArea();
-    $('#canvas').append(start);
-    select(start);
+    canvas.append(start);
+    select(start.find('.arg'));
     updateAll();
 }
 
@@ -13,26 +29,19 @@ function updateAll() {
 
 function elementArea(tmp) {
     if (!tmp) tmp= $('<div class="box"/>');
-    tmp.draggable({
-	    start: handleDragStartEvent,
-		distance: 20
-    });
-    tmp.click(handleSelectionEvent);
+    tmp.attr('onmousedown','msDown(evt)');
     return tmp;
 }
 
 function bodyArea() {
-    tmp= dropArea();
-    tmp.addClass('cmd');
-    return tmp;
+    var div= $('<div class="box-body"/>');
+    div.append(dropArea());
+    return div;
 }
 
 function dropArea() {
     var tmp= $('<div class="box arg">?</div>');
-    tmp.droppable({
-	    drop: handleDropEvent
-    });
-    tmp.click(handleSelectionEvent);
+    tmp.attr('onmousedown','msDown(evt)');
     return tmp;
 }
 
@@ -69,39 +78,90 @@ function selectNext(obj) {
     unselectAll();
 }
 
-function handleSelectionEvent(event) {
-    var target= $(event.target);
-    while(!target.hasClass('box'))
-	    target= target.parent();
-    if (target.hasClass('selected')) {
-	    target.removeClass('selected');
-	} else {
-        select(target);
+
+// msDown is called whenever the mouse button is pressed anywhere on the root document.
+function msDown (evt) {
+    evt.preventDefault();
+    if (hand==null && evt.target!=null) {
+        // find signaling object
+        var grabbed= $(evt.target);
+    	while (!grabbed.hasClass('box')) {
+            if (grabbed.attr('id')=='canvas') return;
+	        grabbed= grabbed.parent();
+	    }
+	    hand= grabbed;
+        unselect= hand.hasClass('selected');
+        select(hand);
+        updateMenu();
+
+	    // store mouse position. Will be updated when mouse moves.
+        startPos= [evt.clientX, evt.clientY];
+
+        hasMoved= false;
+        return false;
     }
-    updateMenu();
+}
+
+function msMove(evt) {
+    evt.preventDefault();
+    if (hand) {
+        var dx= evt.clientX - startPos[0];
+        var dy= evt.clientY - startPos[1];
+        if (hasMoved || Math.abs(dx)+Math.abs(dy)>20) {
+            if (!hasMoved) {
+                while (!hand.hasClass('box') || hand.hasClass('arg')) {
+                    if (hand.attr('id')=='canvas') return;
+                    hand = hand.parent();
+                }
+                startOffset= hand.offset();
+                if (!hand.hasClass('float')) {
+                    if (hand.attr('data-type') == 'ident') {
+                        var clone = hand.clone();
+                        clone.removeClass('selected');
+                        hand.before(clone);
+                    }
+                    else {
+                        hand.before(dropArea());
+                    }
+                    hand.addClass('float');
+                    $('canvas').append(hand);
+                    updateAll();
+                }
+                hand.css('pointer-events','none');
+                hasMoved= true;
+            }
+
+            if (hasMoved) {
+                var arg= $(evt.target);
+                if (arg.hasClass('arg') && !arg.parents().is(hand)) {
+                    console.log('DROP');
+                    arg.replaceWith(hand);
+                    hand.removeClass('float');
+                    updateAll();
+                    startPos= [evt.clientX, evt.clientY];
+                    hasMoved= false;
+                }
+            }
+            if (hasMoved) {
+                hand.offset({
+                    top: startOffset.top + dy, 
+                    left: startOffset.left + dx
+                });
+            }
+        }
+    }
+}
+
+function msUp (evt) {
+    evt.preventDefault();
+    if (hand) {
+        hand.css('pointer-events', 'all');
+        if (unselect && !hasMoved) {
+            hand.removeClass('selected');
+            updateMenu();
+        }
+        hand= null;
+    }
     return false;
 }
 
-function handleDragStartEvent(event) {
-    var target= $(event.target);
-    if (! target.hasClass('float')) {
-	if (target.attr('data-type')=='ident') {
-	    var clone= target.clone();
-	    clone.removeClass('selected');
-	    target.before(elementArea(clone));
-	} else {
-	    target.before(dropArea());
-	}
-	target.addClass('float');
-	$('#canvas').append(target);
-	updateAll();
-    }
-}
-
-function handleDropEvent(event, ui) {
-    ui.draggable.removeClass('float');
-    ui.draggable.css("left","0px"); 
-    ui.draggable.css("top","0px"); 
-    $(event.target).replaceWith(ui.draggable);
-    updateAll();
-}
