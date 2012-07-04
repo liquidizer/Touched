df94e4645426a0b52a437dc052160109d9e3273f
Stefan Dirnstorfer <stefan@dadim.de>
2012-07-04 14:24:47 +0200

    first version: viz

--- touched-tocode.js
+++ touched-tocode.js
@@ -8,18 +8,27 @@ function toCode(node, commands) {
     }
     return {
         args: function(name, force) { 
-            var arglist= list.filter( function() {
-                return !name || $(this).attr('data-name')==name;
-            })
-	    arglist.each( function(i,obj) {
-	        if(force && $(obj).children().length==0) {
-		    try {
-			markError($(obj).attr('id'), "Missing argument");
-		    } catch(e) {
-			throw "Missing argument: "+name;
+	    // find attributes with the name
+            var arglist= list;
+	    if (name)
+		arglist= arglist.filter( function() {
+			return $(this).attr('data-name')==name;
+		    });
+	    // mark errors if the force flag is provided
+	    if (force) {
+		arglist.each( function(i,obj) {
+		    if($(obj).children().length==0) {
+			try {
+			    markError($(obj).attr('id'), "Missing argument");
+			} catch(e) {
+			    throw "Missing argument: "+name;
+			}
 		    }
-		}
-	    });
+		});
+		if (arglist.length==0)
+		    throw "Invalid arg : "+this.type+" -> "+name;
+	    }
+	    // return child elements
 	    return arglist.children().filter(':not(.error)')
 		.map( function(i,obj) { return toCode($(obj), commands); });
         },
@@ -27,20 +36,26 @@ function toCode(node, commands) {
         node: node,
         type: node.attr('data-type'),
         text: node.is('.box-text') ? node.text() : undefined,
-	call: function(data) {
+	call: function(data, callback) {
 	    if (commands) {
 		var f= commands;
-		this.type.split('.').forEach(function(sec) { 
-			f=f[sec]; 
+		var type= this.type
+		type.split('.').forEach(function(sec) { 
+			f=f[sec];
+			if (!f) throw "Undefined function: "+type;
 		    });
-		if (f)
-		    return f(this, data);
-		else
-		    throw "Undefined function: "+this.type;
+		return f(this, data, callback);
 	    }
         },
-	assert: function(test, message) {
-	    if (!test) this.error(message);
+	fold : function(arg, data, callback) {
+	    var list= this.args(arg).toArray();
+	    var f= function(data2) {
+		if (list.length)
+		    list.shift().call(data2, f);
+		else
+		    callback(data2);
+	    };
+	    f(data);
 	},
 	error: function(message) {
 	    markError(this.node.attr('id'), message);
