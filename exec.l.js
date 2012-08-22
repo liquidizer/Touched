98f0ca16aec44008f83625fd5ad315f852005fe9
Stefan Dirnstorfer <stefan@thetaris.com>
2012-08-22 16:37:18 +0200

    Debugging L-Systems

--- exec.l.js
+++ exec.l.js
@@ -1,12 +1,9 @@
-lsys_debug = false;
-lsys_debug_expand = false;
-lsys_debug_plot = false;
-lsys_debug_perf = true;
+var plotter;
+var rules;
 
 lsys_move_count = 0;
 lsys_move_length = 0;
-lsys_move_limit_enabled = false;
-lsys_move_limit = 1000;
+lsys_call_depth = 0;
 
 lsys_plot_id = 0;
 lsys_last_fit = 0;
@@ -19,43 +16,117 @@ lsys_style += " #lsystem .decorated {stroke-linecap: round;}";
 
 commands.l= {
     system : function(code, output) {
-		lsys_plot_id++;
-		lsys_move_count = 0;
-		lsys_move_length = 0;
-		
-		iterations = parseInt(code.arg('iterations').text || "0");
-		if (iterations<0) {
-			code.arg('iterations').error("iterations must be positive");
-			return;
+	lsys_plot_id++;
+	lsys_move_count = 0;
+	lsys_move_length = 0;
+	
+	iterations = parseInt(code.arg('iterations').text || "0");
+	if (iterations<0) {
+	    code.arg('iterations').error("iterations must be positive");
+	    return;
+	}
+	
+	output.selectAll('*').remove();
+	
+	var svg = output.append("svg").attr('id', 'lsystem');
+	
+	var strokeColor = code.arg('stroke').text || lsys_stroke;
+	lsys_custom_style = " #lsystem line {stroke: " + strokeColor + ";}";
+	svg.append("style").attr('type','text/css').text(lsys_style + lsys_custom_style);
+	
+	plotter = new Plotter(svg);
+	rules = code.args('rule');
+
+	var tracker = new Tracker(svg[0][0].createSVGMatrix(), strokeColor);
+	tracker.current_plot_id= lsys_plot_id;
+	tracker.it = new Iterator([code.arg('axiom')], iterations);
+	
+	plotLSys(tracker,
+		 function() { // callback for final fitting of plot into svg viewBox
+		     plotter.decorate();
+		 });
+    },
+    op : {
+	rotate : function(code, tracker, callback) {
+	    var angle = getNumber(code.arg('angle')) || 0;
+	    tracker.matrix = tracker.matrix.rotate(angle);
+	    callback(tracker);
+	},
+	color : function(code, tracker, callback) {
+	    var c = code.arg('color').text || 'black';
+	    tracker.strokeColor = c;
+	    callback(tracker);
+	},
+	scale : function(code, tracker, callback) {
+	    var f = getNumber(code.arg('factor')) || 1.0;
+	    tracker.matrix = tracker.matrix.scale(f);
+	    callback(tracker);
+	},
+	scalenu : function(code, tracker, callback) {
+	    var fx = getNumber(code.arg('factorX')) || 1.0;
+	    var fy = getNumber(code.arg('factorY')) || 1.0;
+	    tracker.matrix = tracker.matrix.scaleNonUniform(fx, fy);
+	    callback(tracker);
+	},
+	move : function(code, tracker, callback) {
+	    // actual drawing happens here
+	    var len = getNumber(code.arg('length')) || 1.0;
+	    var m= tracker.matrix;
+	    var line = plotter.root.append('line')
+		.attr('x2', len)
+		.attr('transform', "matrix("+m.a+","+m.b+","+m.c+","+m.d+","+m.e+","+m.f+")")
+	        .style('stroke', tracker.strokeColor);
+	    tracker.matrix = tracker.matrix.translate(len, 0);
+	    lsys_move_count++;
+	    lsys_move_length += (+len); // Note: unary + is the fastest way to coerce len to Number type
+	    callback(tracker);
+	},
+	group : function(code, tracker, callback) {
+	    // for groups, recursively plot content, but use a clone of the current transform matrix
+	    var children = code.args('child');
+	    var tracker2 = tracker.clone();
+	    tracker2.it = new Iterator(children, tracker.it.iteration);
+	    plotLSys(tracker2, function() { // after plotting group contents...
+		plotLSys(tracker, callback); // ...continue current plotting iteration with next element
+	    });
+	},
+	variable : function(code, tracker, callback) {
+	    // expand variable and plot the expanded result
+	    var expanded = []; // store result of this iteration's rules application
+	    var varName = code.arg('name').text;
+	    for (r in rules) {
+		var rule = rules[r];
+		var r_in = rule.arg('in');
+		var r_outs = rule.args('out');
+		if (varName == r_in.arg('name').text) {
+		    expanded = expanded.concat(r_outs);
 		}
-		
-		output.selectAll('*').remove();
-		
-		var svg = output.append("svg").attr('id', 'lsystem');
-		
-		var strokeColor = code.arg('stroke').text || lsys_stroke;
-		lsys_custom_style = " #lsystem line {stroke: " + strokeColor + ";}";
-		svg.append("style").attr('type','text/css').text(lsys_style + lsys_custom_style);
-		
-		var plotter = new Plotter(svg);
-		var tracker = new Tracker(svg[0][0].createSVGMatrix(), strokeColor);
-		var it = new Iterator([code.arg('axiom')], iterations);
-		var rules = code.args('rule');
-		
-		var startTime = (lsys_debug_perf) ? new Date().getTime() : 0;
-		
-		plotLSys(plotter, tracker, it, rules, 
-			function() { // callback for final fitting of plot into svg viewBox
-				plotter.decorate();
-				if (lsys_debug_perf) {
-					console.log("rendering finished in " + (new Date().getTime()-startTime) + " ms");
-				}
-			}, 
-			function() { // callback for aborting
-				plotter.decorate();
-				if (lsys_debug) console.log("rendering aborted");
-			}
-		);
+	    }
+	    var it0= tracker.it;
+	    tracker.it = new Iterator(expanded, tracker.it.iteration-1);
+	    var current_plot_id = lsys_plot_id;
+	    var nextStep= function() {
+		var timestamp = new Date().getTime();
+		if (timestamp-300 > lsys_last_fit) { // every 200 ms...
+		    plotter.fit(); // adapt svg viewBox to current plot size
+		    lsys_last_fit = timestamp;
+		}
+		if (timestamp-1000 > lsys_last_fill) { // every second...
+		    plotter.fill(); // adapt line stroke width to scale of current plot size
+		    lsys_last_fill = timestamp;
+		}
+		plotLSys(tracker, function() { // after plotting expansion...
+		    tracker.it= it0;
+		    plotLSys(tracker, callback); 
+		});
+	    };
+	    if (lsys_call_depth++ > 100) {
+		lsys_call_depth= 0;
+		setTimeout(nextStep, 1);
+	    } else {
+		nextStep();
+	    }
+	}
     }
 }
 
@@ -83,9 +154,6 @@ function Plotter(svg) {
 			//lines.style("stroke-width", sw);
 			var root = this.svg.select("#lsystem-root");
 			root.style("stroke-width", sw); // styling only the root g element instead of each line reduces SVG disk footprint ALOT
-			if (lsys_debug_plot) console.log("moves="+lsys_move_count + "   length="+lsys_move_length);
-			if (lsys_debug_plot) console.log("area="+area + "   ff="+fillFactor + "   -> sw="+sw);
-			if (lsys_debug_plot) console.log("changed stroke width to " + sw);
 			lsys_stroke_width = sw;
 		}
 	}
@@ -113,8 +181,9 @@ function Tracker(matrix, strokeColor) {
 	this.strokeColor = strokeColor;
 	// clone method needed to fork group transforms
 	this.clone = function() {
-		var clone = new Tracker(this.matrix, this.strokeColor);
-		return clone;
+	    var clone = new Tracker(this.matrix, this.strokeColor);
+	    clone.current_plot_id= this.current_plot_id;
+	    return clone;
 	}
 }
 
@@ -148,22 +217,10 @@ function getNumber(code, useInteger) {
 	if (code.type=='number') {
 		return code.text;
 	} else if (code.type=='math.random') {
-		var min = +(code.arg('min').text || 0);
-		var max = +(code.arg('max').text || 1);
-		if (useInteger) {
-			return getRandomInteger(min, max);
-		} else {
-			return getRandomFloat(min, max);
-		}
+	    var min = +(code.arg('min').text || 0);
+	    var max = +(code.arg('max').text || 1);
+	    return getRandomFloat(min, max);
 	}
-	return undefined;
-}
-
-// generates random integer in range [min, max], i.e. inclusive
-function getRandomInteger(min, max) {
-	var r = min + Math.floor(Math.random()*(max-min+1)); // +1 so max will actually be included in interval
-	//console.log("generating random number between " + min + " and " + max + ": " + r);
-	return r;
 }
 
 // generates random float in range [min, max), i.e. exclusive
@@ -173,113 +230,14 @@ function getRandomFloat(min, max) {
 	return r;
 }
 
-function plotLSys(plotter, tracker, iterator, rules, callback, abort) {
-	if (iterator.iteration<0) {
-		callback();
-		return;
-	}
-	if (lsys_debug) console.log("plot iteration "+(iterations-iterator.iteration));
-	while (iterator.hasNext()) {
-		var elem = iterator.next();
-		if (!elem) {
-			callback();
-		} else {
-			if (lsys_debug_plot) console.log("elem: " + elem.type);
-			if (elem.type == 'l.op.variable') {
-				// expand variable and plot the expanded result
-				if (lsys_debug) console.log("expand iteration "+(iterations-iterator.iteration));
-				var expanded = []; // store result of this iteration's rules application
-				var varName = elem.arg('name').text;
-				if (lsys_debug_expand) console.log("expanding var " + varName);
-				for (r in rules) {
-					var rule = rules[r];
-					var r_in = rule.arg('in');
-					var r_outs = rule.args('out');
-					if (varName == r_in.arg('name').text) {
-						expanded = expanded.concat(r_outs);
-					}
-				}
-				if (lsys_debug_expand) console.log(expanded);
-				
-				var it2 = new Iterator(expanded, iterator.iteration-1);
-				var current_plot_id = lsys_plot_id;
-				setTimeout(function() {
-					if (lsys_plot_id == current_plot_id) {
-						var timestamp = new Date().getTime();
-						if (timestamp-300 > lsys_last_fit) { // every 200 ms...
-							plotter.fit(); // adapt svg viewBox to current plot size
-							lsys_last_fit = timestamp;
-						}
-						if (timestamp-1000 > lsys_last_fill) { // every second...
-							plotter.fill(); // adapt line stroke width to scale of current plot size
-							lsys_last_fill = timestamp;
-						}
-						plotLSys(plotter, tracker, it2, rules, function() { // after plotting expansion...
-							plotLSys(plotter, tracker, iterator, rules, callback, abort); // ...continue current plotting iteration with next element
-						}, abort);
-					} else {
-						if (lsys_debug) console.log("l-system is deprecated");
-						abort();
-					}
-				}, 1);
-				return;
-			}
-			else if (elem.type == 'l.op.group') {
-				// for groups, recursively plot content, but use a clone of the current transform matrix
-				var children = elem.args('child');
-				var clone = tracker.clone();
-				var it2 = new Iterator(children, iterator.iteration);
-				plotLSys(plotter, clone, it2, rules, function() { // after plotting group contents...
-					plotLSys(plotter, tracker, iterator, rules, callback, abort); // ...continue current plotting iteration with next element
-				}, abort);
-				return;
-			}
-			else if (elem.type == 'l.op.move') {
-				// actual drawing happens here
-				var len = getNumber(elem.arg('length'), true) || 0;
-				if (len == 0) continue; // for undefined or zero length we do nothing
-				var line = plotter.root.append('line')
-				.attr('x2', len)
-				.attr('transform', "matrix("+tracker.matrix.a+","+tracker.matrix.b+","+tracker.matrix.c+","+tracker.matrix.d+","+tracker.matrix.e+","+tracker.matrix.f+")");
-				if (tracker.strokeColor != lsys_stroke) {
-					line.style("stroke", tracker.strokeColor);
-				}
-				tracker.matrix = tracker.matrix.translate(len, 0);
-				if (lsys_debug_plot) console.log("move " + len);
-				lsys_move_count++;
-				lsys_move_length += (+len); // Note: unary + is the fastest way to coerce len to Number type
-				if (lsys_move_limit_enabled && (lsys_move_count >= lsys_move_limit)) {
-					console.log("move limit reached at " + lsys_move_limit);
-					abort();
-					return;
-				}
-			}
-			else if (elem.type == 'l.op.scale') {
-				var f = getNumber(elem.arg('factor'), false) || 1.0;
-				if (f == 1.0) continue; // for undefined factor or factor 1.0 we do nothing (Note: f is coerced to Number type internally)
-				tracker.matrix = tracker.matrix.scale(f);
-				if (lsys_debug_plot) console.log("scale " + f);
-			}
-			else if (elem.type == 'l.op.scalenu') {
-				var fx = getNumber(elem.arg('factorX'), false) || 1.0;
-				var fy = getNumber(elem.arg('factorY'), false) || 1.0;
-				if ((fx == 1.0) && (fy == 1.0)) continue; // do nothing if both factors are undefined or 1.0
-				tracker.matrix = tracker.matrix.scaleNonUniform(fx, fy);
-				if (lsys_debug_plot) console.log("scalenu " + fx + " " + fy);
-			}
-			else if (elem.type == 'l.op.rotate') {
-				var angle = getNumber(elem.arg('angle'), true) || 0;
-				if (angle == 0) continue; // for undefined or zero angle we do nothing
-				tracker.matrix = tracker.matrix.rotate(angle);
-				if (lsys_debug_plot) console.log("turn " + angle);
-			}
-			else if (elem.type == 'l.op.color') {
-				var c = elem.arg('color').text;
-				if (!c) continue; // for undefined or zero color we do nothing
-				tracker.strokeColor = c; // store new color to use for subsequent moves
-				if (lsys_debug_plot) console.log("color " + c);
-			}
-		}
-	}
+function plotLSys(tracker, callback) {
+    if (lsys_plot_id != tracker.current_plot_id) return;
+    var iterator= tracker.it;
+    if (iterator.iteration<0 || !iterator.hasNext()) {
 	callback();
+    } else {
+	iterator.next().call(tracker, function() {
+	    plotLSys(tracker, callback);
+	});
+    }
 }
